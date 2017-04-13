349ac0dec2daabb8b6f1a50418bc2bf6619d93ca
nulun <nul@live.com>
2017-04-13 20:40:41 +0800

    server side sync

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var recipeSchema = new Schema({
     recipe_name: String,
     recipe_detail: String,
-    recipe_image: {path:String, contentType:String}
+    recipe_image: {path:String,contentType:String}
 });
 
 var Recipe = mongoose.model('Recipe', recipeSchema);
